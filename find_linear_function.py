430fe7433b2a01590bcb9c84b8279a4812a4b96e
Reinhold B <reinhold.brant@gmx.de>
2020-08-22 13:35:46 +0200

    find linear function

---
+++ find_linear_function.py
@@ -0,0 +1,45 @@
+"""
+Given two coordinates, this algorithm will find the linear function
+those two coordinates lay on:
+
+Example:
+X [1, 3], Y [2, 5] --> f(x) = 2x + 1
+
+"""
+
+
+class equationFinder:
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    def validate(self, X, Y):
+        """Validate if the X and Y coordinates aren't the same"""
+        if (X == Y):
+            return False
+
+    def find_slope(self, X, Y):
+        """Find the slope of the function"""
+        self.slope = (Y[1] - X[1]) / (Y[0] - X[0])
+        return round(self.slope) if self.slope % 1 == 0 else self.slope
+
+    def find_axis_intercept(self, slope, X):
+        """Find the y-intercept by solving the equation"""
+        y, x = X[1], X[0]
+        self.axis_intercept = y - self.slope * x
+        if self.axis_intercept % 1 == 0:
+            self.axis_intercept = round(self.axis_intercept)
+        if self.axis_intercept > 0:
+            self.axis_intercept = f"+ {self.axis_intercept}"
+        else:
+            self.axis_intercept = str(self.axis_intercept)
+        return f"{self.axis_intercept}" if self.axis_intercept != "0" else ""
+
+    def find_equation(self):
+        """Call this method to solve find the equation"""
+        if self.validate(self.X, self.Y):
+            slope = self.find_slope(self.X, self.Y)
+            axis_intercept = self.find_axis_intercept(slope, self.X)
+            return f"{slope}x " + (str(axis_intercept))
+
+        return "No function!"
